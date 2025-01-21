2f5fec05e192f16763b7c82e39b75bf5ae9b3d5d
wonajec270 <wonajec270@myweblaw.com>
2025-01-21 03:10:38 +0000

    2025-01-21

--- process.sh
+++ process.sh
@@ -4,7 +4,7 @@ cd /home/demo/hecate/
 cp ../multi_source_data/cro_data/cro_new.json .
 rm cro_new.json.gz
 gzip cro_new.json
-cp ../multi_source_data/data/figure/mdis_CRO_ources.pdf data/figure/
+cp ../multi_source_data/data/figure/mdis_CRO_sources.pdf data/figure/
 cp ../multi_source_data/data/figure/mdis_ipv4_ipv6_roa.pdf data/figure/
 cp ../multi_source_data/data/figure/mdis_ipv4_ipv6.pdf data/figure/
 cp ../multi_source_data/data/figure/mdis_tal.pdf data/figure/
