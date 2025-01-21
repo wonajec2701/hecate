beffd82f8f7ae144689a9c8708bb6fe1274ed3ac
wonajec270 <wonajec270@myweblaw.com>
2025-01-21 03:10:26 +0000

    2025-01-21

---
+++ process.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd /home/demo/hecate/
+cp ../multi_source_data/cro_data/cro_new.json .
+rm cro_new.json.gz
+gzip cro_new.json
+cp ../multi_source_data/data/figure/mdis_CRO_ources.pdf data/figure/
+cp ../multi_source_data/data/figure/mdis_ipv4_ipv6_roa.pdf data/figure/
+cp ../multi_source_data/data/figure/mdis_ipv4_ipv6.pdf data/figure/
+cp ../multi_source_data/data/figure/mdis_tal.pdf data/figure/
+cp ../multi_source_data/data/figure/mdis_validate_compare_ipv4.pdf data/figure/
+cp ../multi_source_data/data/figure/mdis_validate_compare_ipv6.pdf data/figure/
+cp ../multi_source_data/data/figure/mdis_validate_compare_total.pdf data/figure/
+cp ../multi_source_data/data/result/mdis_CRO_analysis data/result/
+cp ../multi_source_data/data/cro data/
+cd data
+rm cro.gz
+gzip cro
+cd ..
+
+current_date=$(date +%Y-%m-%d)
+git add .
+git commit -m "$current_date"
+git push -u origin main
